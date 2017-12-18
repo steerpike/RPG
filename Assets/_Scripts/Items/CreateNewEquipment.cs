1cd1f775aacd16b75c514599fa292baf368bbc64
Dwyer <Sam.Dwyer@sage.com>
2017-12-18 15:25:19 +1100

    Items

---
+++ Assets/_Scripts/Items/CreateNewEquipment.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateNewEquipment : MonoBehaviour {
+
+	private BaseEquipment newEquipment;
+	private string[] itemNames = new string[4]{"Common", "Great", "Amazing", "Legendary"};
+	// Use this for initialization
+	void Start () {
+	
+	}
+	private void ChooseItemName() {
+		newEquipment.ItemName = itemNames [Random.Range (0, 3)] + " ITem";
+	}
+	private void CreateEquipment() {
+		newEquipment = new BaseEquipment ();
+	}
+	void ChooseItemType() {
+	
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/_Scripts/Items/CreateNewWeapon.cs
+++ Assets/_Scripts/Items/CreateNewWeapon.cs
@@ -4,6 +4,13 @@ using System.Collections;
 public class CreateNewWeapon : MonoBehaviour {
 
 	private BaseWeapon newWeapon;
+	/*
+	public void Start() {
+		CreateWeapon ();
+		Debug.Log (newWeapon.ItemName);
+		Debug.Log (newWeapon.WeaponType.ToString());
+	}
+	*/
 
 	public void CreateWeapon() {
 		newWeapon = new BaseWeapon ();
@@ -14,9 +21,22 @@ public class CreateNewWeapon : MonoBehaviour {
 		newWeapon.Intelligence = Random.Range(1,10);
 		newWeapon.Faith = Random.Range(1,10);
 		newWeapon.Agility = Random.Range(1,10);
+		ChooseWeaponType ();
+		newWeapon.SpellEffectID = Random.Range (1, 101);
 
 	}
 	private void ChooseWeaponType() {
-		int randomTemp = Random.Range (1,7);
+		int randomTemp = Random.Range (1,5);
+		if (randomTemp == 1) {
+			newWeapon.WeaponType = BaseWeapon.WeaponTypes.SWORD;
+		} else if (randomTemp == 2) {
+			newWeapon.WeaponType = BaseWeapon.WeaponTypes.STAFF;
+		} else if (randomTemp == 3) {
+			newWeapon.WeaponType = BaseWeapon.WeaponTypes.DAGGER;
+		} else if (randomTemp == 4) {
+			newWeapon.WeaponType = BaseWeapon.WeaponTypes.MACE;
+		} else if (randomTemp == 5) {
+			newWeapon.WeaponType = BaseWeapon.WeaponTypes.BOW;
+		}
 	}
 }
