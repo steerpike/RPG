39e7119f3db85cb8bf57f04c7076b5d5256a3657
Dwyer <Sam.Dwyer@sage.com>
2017-12-19 16:57:54 +1100

    Combat

---
+++ Assets/_Scripts/BasePlayer/BasePlayer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasePlayer {
+
+	private string playerName;
+	private int playerLevel;
+	private BaseCharacterClass playerClass;
+	private int strength;
+	private int intelligence;
+	private int faith;
+	private int agility;
+
+	public string PlayerName {
+		get{ return playerName; }
+		set{ playerName = value; }
+	}
+	public int PlayerLevel {
+		get{ return playerLevel; }
+		set{ playerLevel = value; }
+	}
+	public BaseCharacterClass PlayerClass {
+		get{ return playerClass; }
+		set{ playerClass = value; }
+	}
+	public int Strength {
+		get{ return strength; }
+		set{ strength = value; }
+	}
+	public int Intelligence {
+		get{ return intelligence; }
+		set{ intelligence = value; }
+	}
+	public int Faith {
+		get{ return faith; }
+		set{ faith = value; }
+	}
+	public int Agility {
+		get{ return agility; }
+		set{ agility = value; }
+	}
+}
--- Assets/_Scripts/Items/CreateNewEquipment.cs
+++ Assets/_Scripts/Items/CreateNewEquipment.cs
@@ -7,16 +7,40 @@ public class CreateNewEquipment : MonoBehaviour {
 	private string[] itemNames = new string[4]{"Common", "Great", "Amazing", "Legendary"};
 	// Use this for initialization
 	void Start () {
-	
+		CreateEquipment ();
+		Debug.Log (newEquipment.ItemName);
+		Debug.Log (newEquipment.ItemDescription);
+		Debug.Log (newEquipment.EquipmentType.ToString());
 	}
-	private void ChooseItemName() {
-		newEquipment.ItemName = itemNames [Random.Range (0, 3)] + " ITem";
-	}
+
 	private void CreateEquipment() {
 		newEquipment = new BaseEquipment ();
+		newEquipment.ItemName = itemNames [Random.Range (0, 3)] + " Item";
+		newEquipment.ItemID = Random.Range (1, 101);
+
+		newEquipment.ItemDescription = "This is a new piece of equipment.";
+		newEquipment.Strength = Random.Range(1,10);
+		newEquipment.Intelligence = Random.Range(1,10);
+		newEquipment.Faith = Random.Range(1,10);
+		newEquipment.Agility = Random.Range(1,10);
+
+		ChooseItemType ();
 	}
 	void ChooseItemType() {
-	
+		int randomTemp = Random.Range (1, 6);
+		if (randomTemp == 1) {
+			newEquipment.EquipmentType = BaseEquipment.EquipmentTypes.HELMET;
+		} else if (randomTemp == 2) {
+			newEquipment.EquipmentType = BaseEquipment.EquipmentTypes.CLOAK;
+		} else if (randomTemp == 3) {
+			newEquipment.EquipmentType = BaseEquipment.EquipmentTypes.BREASTPLATE;
+		} else if (randomTemp == 4) {
+			newEquipment.EquipmentType = BaseEquipment.EquipmentTypes.BOOTS;
+		} else if (randomTemp == 5) {
+			newEquipment.EquipmentType = BaseEquipment.EquipmentTypes.RING;
+		} else if (randomTemp == 6) {
+			newEquipment.EquipmentType = BaseEquipment.EquipmentTypes.NECKLACE;
+		}
 	}
 	// Update is called once per frame
 	void Update () {
